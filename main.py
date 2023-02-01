2b8e4e2f8d9d9a490a112196a54eaac9b27a0cba
plabondatta26 <plabondatta26@gmail.com>
2023-02-01 23:21:42 +0600

    recaptcha ss added

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ driver = webdriver.Chrome()
 
 url = "https://justpaste.it/"
 
+file = open('aaa.html')
+data = file.read()
 driver.get(url)
 time.sleep(5)
 driver.find_element(by=By.XPATH, value="//input[@placeholder='Title']").send_keys("nfjdshbfjhdbshj")
 
-text_area = driver.find_element(by=By.CLASS_NAME, value="tox-edit-area")
-text_area.click()
-driver.execute_script("arguments[0].textContent = arguments[1];", text_area, "This is a test")
+text_area = driver.find_element(by=By.TAG_NAME, value="iframe").send_keys(data)
+# text_area = driver.find_element(by=By.CLASS_NAME, value="tox-edit-area")
+# text_area.click()
+# driver.execute_script("arguments[0].textContent = arguments[1];", text_area, "This is a test")
 
 driver.find_element(by=By.CLASS_NAME, value="publishButton").click()
+time.sleep(2)
+captcha = driver.find_element(by=By.CLASS_NAME, value="captchaGridContainer").get_attribute('outerHTML')
+correct_word = driver.find_element(by=By.CLASS_NAME, value="correctWord").text
+print(captcha)
+print("-------------------")
+print(correct_word)
 time.sleep(5)
+print(driver.current_url)
